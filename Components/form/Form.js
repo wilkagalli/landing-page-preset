b643ab1257546aaa5fdd8a90ce6f631550d533fa
Wilka <galliwilka@gmail.com>
2021-02-23 00:17:07 -0300

    atualizando

--- Components/form/Form.js
+++ Components/form/Form.js
@@ -1,5 +1,6 @@
 import React from "react";
 import classes from "../form/Form.module.scss";
+import Link from "next/link";
 
 // import { Container } from './styles';
 
@@ -7,10 +8,11 @@ function Form() {
   return (
     <div className={classes.formContainer}>
       <h2 className={classes.title}>Baixe gratuitamente seu Preset!</h2>
-
-      <a download="NORDESTE-IV.dng" href="/assets/NORDESTE-IV.dng">
-        <button className={classes.button}>Baixar Agora</button>
-      </a>
+      <Link href="/assets/NORDESTE-IV.dng">
+        <a>
+          <button className={classes.button}>Baixar Agora</button>
+        </a>
+      </Link>
       <br />
       <br />
       <br />
