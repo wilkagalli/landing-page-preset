6a466e869732ff4d76a56f0c9b6531ab6dc1bd27
Wilka <galliwilka@gmail.com>
2021-02-23 01:40:29 -0300

    enviar email

--- Components/form/Form.js
+++ Components/form/Form.js
@@ -1,16 +1,47 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import classes from "../form/Form.module.scss";
 
 // import { Container } from './styles';
 
 function Form() {
+  const inputRef = useRef();
+  const [emailEnviado, setEmailEnviado] = useState(false);
+
+  async function enviarEmail() {
+    const retorno = await fetch(
+      "https://api.convertkit.com/v3/forms/2061639/subscribe",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: {
+          api_key: "TAwh8Ii6XMTIXM-A4TMe0A",
+          email: inputRef.current.value,
+        },
+      }
+    );
+    setEmailEnviado(true);
+    console.log(retorno);
+  }
+
   return (
     <div className={classes.formContainer}>
-      <h2 className={classes.title}>Baixe gratuitamente seu Preset!</h2>
+      {!emailEnviado ? (
+        <>
+          <h2 className={classes.title}>Baixe gratuitamente seu Preset!</h2>
+          <form className={classes.form}>
+            <input ref={inputRef} placeholder="Endereço de e-mail"></input>
+          </form>
+          <button onClick={enviarEmail} className={classes.button}>
+            Baixar Agora
+          </button>{" "}
+        </>
+      ) : (
+        <h1>
+          O preset foi enviado para seu e-mail. Não esqueça de verificar a caixa
+          de spam :)
+        </h1>
+      )}
 
-      <a target="_blank" href="/assets/NORDESTE-IV.dng">
-        <button className={classes.button}>Baixar Agora</button>
-      </a>
       <br />
       <br />
       <br />
