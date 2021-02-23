371b128db08c3a476cef46c94104e88da698ce9f
Wilka <galliwilka@gmail.com>
2021-02-23 00:30:36 -0300

    atualizando de novo

--- Components/form/Form.js
+++ Components/form/Form.js
@@ -4,15 +4,38 @@ import Link from "next/link";
 
 // import { Container } from './styles';
 
+const download = (url, name) => {
+  if (!url) {
+    throw new Error("Resource URL not provided! You need to provide one");
+  }
+
+  fetch(url)
+    .then((response) => response.blob())
+    .then((blob) => {
+      const blobURL = URL.createObjectURL(blob);
+      const a = document.createElement("a");
+      a.href = blobURL;
+      a.style = "display: none";
+
+      if (name && name.length) a.download = name;
+      document.body.appendChild(a);
+      a.click();
+    })
+    .catch(() => null);
+};
+
 function Form() {
   return (
     <div className={classes.formContainer}>
       <h2 className={classes.title}>Baixe gratuitamente seu Preset!</h2>
-      <Link href="/assets/NORDESTE-IV.dng">
-        <a>
-          <button className={classes.button}>Baixar Agora</button>
-        </a>
-      </Link>
+
+      <button
+        onClick={() => download("/assets/NORDESTE-IV.dng", "NORDEST-IV.dng")}
+        className={classes.button}
+      >
+        Baixar Agora
+      </button>
+
       <br />
       <br />
       <br />
